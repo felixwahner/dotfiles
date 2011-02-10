4055349feab979b7f8d23ca8cf5e3b1c1efff2f5
Walter Souto <wsouto@gmail.com>
2011-02-10 20:51:25 -0200

    Initial import

---
+++ install.sh
@@ -0,0 +1,18 @@
+# Copy files to home directory
+cp files/bash_completion.sh ~/.bash_completion.sh
+cp files/bash_profile ~/.bash_profile
+cp files/gem_completion.sh ~/.gem_completion.sh
+cp files/git_completion.sh ~/.git_completion.sh
+cp files/gitconfig ~/.gitconfig
+cp files/irbrc ~/.irbrc
+cp files/inputrc ~/.inputrc
+cp files/gemrc ~/.gemrc
+cp files/rvmrc ~/.rvmrc
+cp files/gitignore ~/.gitignore
+
+# Need this
+sudo mkdir -p /etc/bash_completion
+
+# Set the shell
+source ~/.bash_profile
+
