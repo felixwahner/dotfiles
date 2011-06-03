dc5b13b421cdd9e38f08e6460036169055f25cbe
Walter Souto <wsouto@gmail.com>
2011-06-03 17:27:33 -0300

    Adjusted install script accordingly

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 # Copy files to home directory
-cp files/bash_completion.sh ~/.bash_completion.sh
 cp files/bash_profile ~/.bash_profile
-cp files/gem_completion.sh ~/.gem_completion.sh
-cp files/git_completion.sh ~/.git_completion.sh
+cp files/bashrc ~/.bashrc
 cp files/gitconfig ~/.gitconfig
 cp files/irbrc ~/.irbrc
 cp files/inputrc ~/.inputrc
@@ -10,11 +8,5 @@ cp files/gemrc ~/.gemrc
 cp files/rvmrc ~/.rvmrc
 cp files/gitignore ~/.gitignore
 
-# Need this
-if [ ! -d /etc/bash_completion ]; then
-    sudo mkdir -p /etc/bash_completion
-fi
-
 # Set the shell
 source ~/.bash_profile
-
