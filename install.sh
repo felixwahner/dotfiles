e33bb56c9fd26557e04f60c44f3aee448c96d149
Walter Souto <wsouto@gmail.com>
2011-06-03 17:42:57 -0300

    Added some informative message to the script

--- install.sh
+++ install.sh
@@ -8,5 +8,25 @@ cp files/gemrc ~/.gemrc
 cp files/rvmrc ~/.rvmrc
 cp files/gitignore ~/.gitignore
 
-# Set the shell
+# Get the whole shebang now!
 source ~/.bash_profile
+
+cat <<EOM
+
+-----------------------------------------------
+You might want to install this using Homebrew:
+
+$ brew install bash-completion
+$ brew install git
+
+If you have XCode4 installed* then you need to
+alter your path to use brew's version of git.
+In order to do that, edit the /etc/paths file
+and put /usr/local/bin before /usr/bin. 
+
+* This is because XCode4 installs a git version
+in the /usr/bin directory. Maybe it's better to
+keep it.
+-----------------------------------------------
+
+EOM
